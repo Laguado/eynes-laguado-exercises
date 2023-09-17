c89636e65f56dd689aa951686a3b34b568f99098
laguado <tms2620.laguado@gmail.com>
2023-09-17 00:12:29 -0300

    function with a dictionary list returns two keys

--- simple/simple.py
+++ simple/simple.py
@@ -1,9 +1,14 @@
 import random
 
-
-def simple_list():
-    pass
-
-
+ # Generate a list of ten dictionaries with unique id and random age
+def simple_list():   
+    result = []
+    for i in range(1,11):
+        result.append({"id":i,"age": random.randint(1,100)})
+        return result
+    
+ 
 def sort_list():
     pass
+    
+    
