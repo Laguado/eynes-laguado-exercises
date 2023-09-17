acceb5f631e09abbd7950bd870a6e06969bf9381
laguado <tms2620.laguado@gmail.com>
2023-09-16 22:58:01 -0300

    Instance, returns and modifies the radius of the circle

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,18 @@
 import math
 
-class Circle():
-    pass
+class Circle:
+     def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius must be greater than 0.")
+        self.radius = radius
+
+     def get_radius(self):
+        return self.radius
+
+     def set_radius(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius must be greater than 0.")
+        self.radius = radius
+
+    
+    
